c112c8acb3552dad881a03aa709b1d6ce09c4f03
serenetangerine <matthew.hall@serenetangerine.com>
2020-02-02 20:14:56 +0000

    getting flask set up out of the box

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Script and config file to set up a LAMPy server in WSL Ubuntu.
 
 To enable WSL run Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux in Powershell then install Ubuntu through the Windows Store.
+
+Documentation for setting up flask inside of apache can be found here:
+  https://www.bogotobogo.com/python/Flask/Python_Flask_HelloWorld_App_with_Apache_WSGI_Ubuntu14.php
--- web-server.sh
+++ web-server.sh
@@ -5,12 +5,13 @@ yes | sudo apt update
 yes | sudo apt upgrade
 
 ## install packages 
-sudo apt install apache2 mysql-server python3-pip 
+sudo apt install apache2 libapache2-mod-wsgi mysql-server python3-pip 
 sudo pip3 install pymysql flask
 
 ## configure apache
 sudo a2dismod mpm_event
-sudo a2enmod mpm_prefork cgi
+sudo a2enmod mpm_prefork wsgi
+#sudo a2enmod cgi
 
 sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.orig
 sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
