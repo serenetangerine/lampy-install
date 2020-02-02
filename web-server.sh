022a7a4ad5f51deb6dffafb13e40f8c9ead1e062
serenetangerine <matthew.hall@serenetangerine.com>
2020-02-02 18:26:11 +0000

    bug fixes

--- web-server.sh
+++ web-server.sh
@@ -6,14 +6,17 @@ yes | sudo apt upgrade
 
 ## install packages 
 sudo apt install apache2 mysql-server python3-pip 
-sudo pip3 install pymysql
+sudo pip3 install pymysql flask
 
 ## configure apache
 sudo a2dismod mpm_event
 sudo a2enmod mpm_prefork cgi
 
 sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.orig
-sudo cp 000-default.conf /etc/apache2/sites/enabled/000-default.conf
+sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
+
+sudo mv /var/www/html/index.html /var/www/html/index.orig
+sudo cp index.py /var/www/html/index.py
 
 sudo service apache2 restart
 
