4cf66a4b6b000aab224dcd8542be4871061fdc30
serenetangerine <matthew.hall@serenetangerine.com>
2020-02-03 02:56:40 +0000

    more flask stuff

--- web-server.sh
+++ web-server.sh
@@ -5,19 +5,23 @@ yes | sudo apt update
 yes | sudo apt upgrade
 
 ## install packages 
-sudo apt install apache2 libapache2-mod-wsgi mysql-server python3-pip 
+yes | sudo apt install apache2 libapache2-mod-wsgi mysql-server python3-pip 
 sudo pip3 install pymysql flask
 
 ## configure apache
-sudo a2dismod mpm_event
-sudo a2enmod mpm_prefork wsgi
-#sudo a2enmod cgi
+#sudo a2dismod mpm_event
+#sudo a2enmod mpm_prefork cgi
+sudo a2enmod wsgi
 
-sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.orig
-sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
+#sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.orig
+#sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
+sudo cp app.conf /etc/apache2/sites-available/app.conf
+
+sudo a2ensite app.conf
 
 sudo mv /var/www/html/index.html /var/www/html/index.orig
-sudo cp index.py /var/www/html/index.py
+sudo cp app.py /var/www/html/app.py
+sudo cp app.wsgi /var/www/html/app.wsgi
 
 sudo service apache2 restart
 
