a50c5bd82f73ba1d4a5b554bd80d841c6ea39b57
serenetangerine <matthew.hall@serenetangerine.com>
2020-02-03 04:52:19 +0000

    fixed flask install. needed libapache2-mod-wsgi-py3 instead of libapache2-mod-wsgi

--- web-server.sh
+++ web-server.sh
@@ -5,23 +5,24 @@ yes | sudo apt update
 yes | sudo apt upgrade
 
 ## install packages 
-yes | sudo apt install apache2 libapache2-mod-wsgi mysql-server python3-pip 
+yes | sudo apt install apache2 libapache2-mod-wsgi-py333 mysql-server python3-pip 
 sudo pip3 install pymysql flask
 
 ## configure apache
-#sudo a2dismod mpm_event
-#sudo a2enmod mpm_prefork cgi
 sudo a2enmod wsgi
 
-#sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.orig
-#sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
-sudo cp app.conf /etc/apache2/sites-available/app.conf
-
-sudo a2ensite app.conf
+sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.orig
+sudo cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
 
 sudo mv /var/www/html/index.html /var/www/html/index.orig
 sudo cp app.py /var/www/html/app.py
 sudo cp app.wsgi /var/www/html/app.wsgi
+
+## make /static and /templates directories and change ownership
+sudo mkdir /var/www/html/static
+sudo mkdir /var/www/html/templates
+
+sudo chown -R www-data:www-data /var/www/html
 
 sudo service apache2 restart
 
